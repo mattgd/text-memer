327069a121ea904c92f69d60284d900855d3d89d
mattgd <mattdzwonczyk@gmail.com>
2019-12-15 16:20:39 -0500

    Add tests and convert to Typescript.

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,60 @@
 'use strict';
 
-var qs = require('querystring')
+import { APIGatewayEvent } from "aws-lambda";
+var qs = require('querystring');
 
 /**
  * Converts the text input to "lacey" formatted text.
  * For example: This is a test -> t H i S i S a T e S t
  * @param {string} text The text to format.
+ * @returns {string} The formatted text.
  */
-const toLacey = text => {
-  text = text.split("");
+const toLacey: (text: string) => string = (text) => {
+  let charArr = text.split("");
   var count = 0;
-
-  for (var i = 0; i < text.length; i++) {
-    // Case insensitive RegEx
-    if (/^[A-Z]$/i.test(text[i])) {
+  
+  return charArr.map((char: string) => {
+    if (/^[A-Z]$/i.test(char)) {
       if (count % 2) {
-        text[i] = text[i].toUpperCase();
+        char = char.toUpperCase();
       } else {
-        text[i] = text[i].toLowerCase();
+        char = char.toLowerCase();
       }
 
       count++;
     }
-  }
 
-  return text.join('');
+    return char;
+  }).join('');
 }
 
 /**
  * Converts the text input to "bum" formatted text.
  * For example: This is a test -> T H I S I S A T E S T
  * @param {string} text The text to format.
+ * @returns {string} The formatted text.
  */
-const toBum = text => {
-  text = text.split('');
+const toBum: (text: string) => string = (text) => {
+  let textArr = text.split('');
   const n = text.length;
   
   for (var i = n - 1; i >= 0; i--) {
     if (!/\s/.test(text[i]) && (i + 1 < n && !/\s/.test(text[i + 1]))) {
-      text.splice(i + 1, 0, ' ');
+      textArr.splice(i + 1, 0, ' ');
     }
   }
-
-  console.log(text);
   
-  return text.join('').trim().toUpperCase();
+  return textArr.join('').trim().toUpperCase();
 }
 
 /**
  * Converts the text input to "leet" formatted text.
  * For example: This is a test -> 741$ 1$ @ 73$7
  * @param {string} text The text to format.
+ * @returns {string} The formatted text.
  */
-const toLeet = text => {
-  const chars = {
+const toLeet: (text: string) => string = text => {
+  const chars: {[key: string]: string} = {
     a: '@',
     b: '8',
     c: '(',
@@ -68,9 +69,10 @@ const toLeet = text => {
     t: '7',
     z: '2'
   };
-
+  
   for (var ch in chars) {
-    text = text.replace(ch, chars[ch]);
+    var regex = new RegExp(ch, 'gi');
+    text = text.replace(regex, chars[ch]);
   }
   
   return text;
@@ -81,10 +83,10 @@ const toLeet = text => {
  * For example: This is a test -> becomes italicized.
  * @param {string} text The text to format.
  */
-const toQuickMaths = text => '\x1B[3m' + text.trim().toUpperCase() + '\x1B[23m';
+const toQuickMaths: (text: string) => string = text => `_${text.trim().toUpperCase()}_`;
 
-exports.handler = async (event) => {
-  const query = qs.parse(event.body).text;
+exports.handler = async (event: APIGatewayEvent) => {
+  const query = qs.parse(event.body || '').text;
   const style = query.substr(0, query.indexOf(' ')).toLowerCase();
   var text = query.substr(query.indexOf(' ') + 1);
 
---
+++ tests/index.test.js
@@ -0,0 +1,46 @@
+var tm = require('../lib/index');
+
+const TEST_TEXT = 'A comprehensive test! A great TEST indeed, with.punctuation.and.:other stuff';
+
+const generateInputEvent = (style) => ({ body: 'text=' + style + ' ' + TEST_TEXT });
+
+const generateExpectedResponse = (expectedText) => ({
+  statusCode: 200,
+  body: expectedText
+});
+
+test("lacey", async () => {
+  const expectedText = 'a CoMpReHeNsIvE tEsT! a GrEaT tEsT iNdEeD, wItH.pUnCtUaTiOn.AnD.:oThEr StUfF';
+  const event = generateInputEvent('lacey');
+  const output = await tm.handler(event);
+  const expected = generateExpectedResponse(expectedText);
+  
+  expect(output).toEqual(expected);
+});
+
+test("bum", async () => {
+  const expectedText = 'A C O M P R E H E N S I V E T E S T ! A G R E A T T E S T I N D E E D , W I T H . P U N C T U A T I O N . A N D . : O T H E R S T U F F';
+  const event = generateInputEvent('bum');
+  const output = await tm.handler(event);
+  const expected = generateExpectedResponse(expectedText);
+  
+  expect(output).toEqual(expected);
+});
+
+test("leet", async () => {
+  const expectedText = '@ (0mpr343n$1v3 73$7! @ 6r3@7 73$7 1nd33d, w174.pun(7u@710n.@nd.:0743r $7uff';
+  const event = generateInputEvent('leet');
+  const output = await tm.handler(event);
+  const expected = generateExpectedResponse(expectedText);
+
+  expect(output).toEqual(expected);
+});
+
+test("quickmaths", async () => {
+  const expectedText = '_A COMPREHENSIVE TEST! A GREAT TEST INDEED, WITH.PUNCTUATION.AND.:OTHER STUFF_';
+  const event = generateInputEvent('quickmaths');
+  const output = await tm.handler(event);
+  const expected = generateExpectedResponse(expectedText);
+
+  expect(output).toEqual(expected);
+});
